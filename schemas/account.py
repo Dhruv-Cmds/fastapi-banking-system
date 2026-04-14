3c74afdbc1af0255b60890915be1783abd64e115
Dhruv-Cmds <pd1900565@gmail.com>
2026-04-14 17:52:42 +0530

    Added Comments in schemas.py

--- schemas/account.py
+++ schemas/account.py
@@ -6,10 +6,12 @@ class AccountCreate(BaseModel):
     acc_no: str
     balance: Decimal
 
+# For deposit and withdraw
 class Amount(BaseModel):
 
     amount: Decimal
 
+# For transfer 
 class Transfer(BaseModel):
 
     from_account_id: int
--- schemas/user.py
+++ schemas/user.py
@@ -1,5 +1,19 @@
 from pydantic import BaseModel, StringConstraints
+
+''' BaseModel = helps you to define structure how data will store.
+    Its like a set of rule that must be followed.
+
+    StringConstraints = Set rules for string such as minimum or 
+    maximum length of string. '''
+
+# ---------------------------------------------------------------------------------------
+
 from typing import Annotated
+
+''' Annotated = Gives you extra conditions like someof your data will 
+    store as string Annotated also check the length of data (as per amount you pass).'''
+
+# ---------------------------------------------------------------------------------------
 
 passwordStr = Annotated[str, StringConstraints(min_length=4, max_length=72)]
 
