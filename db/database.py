a907322cf651667058faaa18b4a3c00808ae3397
Dhruv-Cmds <pd1900565@gmail.com>
2026-04-14 17:17:04 +0530

    Added Comments in database.py

--- db/database.py
+++ db/database.py
@@ -1,19 +1,12 @@
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker, declarative_base
 from dotenv import load_dotenv
-from urllib.parse import quote_plus
 import os
-from sqlalchemy.orm import declarative_base
+
+# quote_plus = convert special characters into string.
+from urllib.parse import quote_plus
 
 load_dotenv()
-
-def get_db():
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
-
 
 DB_USER = os.getenv("DB_USER")
 DB_PASSWORD = quote_plus(os.getenv("DB_PASSWORD"))
@@ -27,3 +20,13 @@ engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(bind=engine, autocommit=False, autoflush=False)
 
 Base = declarative_base()
+
+def get_db():
+
+    db = SessionLocal() # It Used to read, add, update, delete data.
+
+    try:
+        yield db # Give the database connection to who needs it
+        
+    finally:
+        db.close() # close the database
