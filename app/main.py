f9ca38e39bbef7ba2c58736f94691e65d714571d
Dhruv-Cmds <pd1900565@gmail.com>
2026-04-16 09:59:10 +0530

    Fixed main.py

--- app/main.py
+++ app/main.py
@@ -1,20 +1,31 @@
 from fastapi import FastAPI
 from app.db import engine, Base
+
 from app.models import account, user
 from app.routes import account, auth
 from fastapi.middleware.cors import CORSMiddleware
 
+# Create FastAPI app
 app = FastAPI()
+
+origins = [
+    "http://localhost:5173",     # frontend
+    "http://127.0.0.1:5173"      # backend
+]
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=origins, 
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
 
+
+# Create DB tables (for development only)
 Base.metadata.create_all(bind=engine)
 
+
+# Register routes with prefixes (clean API structure)
 app.include_router(account.router)
 app.include_router(auth.router)
