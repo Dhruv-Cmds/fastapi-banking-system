cb247752a9c38c986fa3c85eb8f5a06c4e9f5536
Dhruv-Cmds <pd1900565@gmail.com>
2026-04-16 15:19:02 +0530

    Refactor auth: integrate OAuth2PasswordBearer for Swagger support and cleaner JWT validation

--- app/dependencies/auth.py
+++ app/dependencies/auth.py
@@ -5,56 +5,86 @@ from app.db import get_db
 from app.models import User
 from app.core import SECRET_KEY, ALGORITHM
 from fastapi import Header
+from fastapi.security import OAuth2PasswordBearer
 
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/api/login")
 
 # Depends(get_db) Opens data Base connection to read, write, delete, update
 
 # takes a token ---> verifies the token, finds the user in data base and return the user  
-def get_current_user(
-        authorization: str = Header(None), 
-        db: Session = Depends(get_db)
-    ):
+# def get_current_user(
+#         authorization: str = Header(None), 
+#         db: Session = Depends(get_db)
+#     ):
 
+#     """
+#     Extracts and verifies JWT token
+#     Returns authenticated user
+#     """
+    
+
+#     #  No token provided
+#     if not authorization:
+#         raise HTTPException(status_code=401, detail="No token")
+
+
+#     #  Wrong format
+#     if not authorization.startswith("Bearer "):
+#         raise HTTPException(status_code=401, detail="Invalid format")
+
+
+#     # Extract token
+#     token = authorization.split(" ")[1]
+
+
+#     try:
+#         # verifies token is valid / checks signature using SECRET_KEY / reads hidden data inside
+#         payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+
+#         #  Extract user ID
+#         user_id = int(payload.get("sub"))
+
+#         if user_id is None:
+#             raise HTTPException(status_code=401, detail="Invalid token payload")
+
+#     except JWSError:
+
+#         # Invalid / expired / tampered token
+#         raise HTTPException(status_code=401, detail="Invalid token")
+    
+#     # Fetch user from DB
+#     user = db.query(User).filter(User.id == user_id).first()
+
+#     if user is None:
+
+#         raise HTTPException(status_code=401, detail="User not found")
+    
+#     return user
+
+def get_current_user(
+    token: str = Depends(oauth2_scheme),
+    db: Session = Depends(get_db)
+):
     """
     Extracts and verifies JWT token
     Returns authenticated user
     """
-    
-
-    #  No token provided
-    if not authorization:
-        raise HTTPException(status_code=401, detail="No token")
-
-
-    #  Wrong format
-    if not authorization.startswith("Bearer "):
-        raise HTTPException(status_code=401, detail="Invalid format")
-
-
-    # Extract token
-    token = authorization.split(" ")[1]
-
 
     try:
-        # verifies token is valid / checks signature using SECRET_KEY / reads hidden data inside
         payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
 
-        #  Extract user ID
         user_id = int(payload.get("sub"))
 
         if user_id is None:
             raise HTTPException(status_code=401, detail="Invalid token payload")
 
     except JWSError:
-
-        # Invalid / expired / tampered token
         raise HTTPException(status_code=401, detail="Invalid token")
-    
-    # Fetch user from DB
+
     user = db.query(User).filter(User.id == user_id).first()
 
     if user is None:
-
         raise HTTPException(status_code=401, detail="User not found")
-    
+
     return user
