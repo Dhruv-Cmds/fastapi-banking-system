8d01336c72eb66c2d541a7aa6aa5d968827e2bec
Dhruv-Cmds <pd1900565@gmail.com>
2026-04-16 10:57:20 +0530

    Updated .env file

--- app/db/database.py
+++ app/db/database.py
@@ -15,6 +15,7 @@ load_dotenv()
 DB_USER = os.getenv("DB_USER")
 DB_PASSWORD = quote_plus(os.getenv("DB_PASSWORD"))
 DB_HOST = os.getenv("DB_HOST")
+DB_PORT = os.getenv("DB_PORT", "3306")
 DB_NAME = os.getenv("DB_NAME")
 
 
@@ -24,7 +25,9 @@ if not all([DB_USER, DB_PASSWORD, DB_HOST, DB_NAME]):
 
 
 # Build database URL
-DATABASE_URL = f"mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}/{DB_NAME}"
+# DATABASE_URL = f"mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}/{DB_NAME}"
+
+DATABASE_URL = f"mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}?ssl=true"
 
 
 #  Create engine with production-safe settings
