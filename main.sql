3d89821f14f5a96359e555fa52fa6e9e4f6fbeee
Dhruv-Cmds <pd1900565@gmail.com>
2026-04-17 11:32:16 +0530

    Fixed main.sql logic

--- main.sql
+++ main.sql
@@ -2,9 +2,9 @@ USE bankaccountsystem;
 
 -- DROP TABLE users;
 -- DROP TABLE accounts;
+-- DROP TABLE transaction;
 
--- DELETE FROM accounts WHERE id = 3;
--- DELETE FROM users WHERE id = 1;
 
 SELECT * FROM users;
 SELECT * FROM accounts;
+SELECT * FROM transaction;
