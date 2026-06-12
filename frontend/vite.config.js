e1cf148ceb55ad291407547ef2f94394341ad913
Dhruv-Cmds <pd1900565@gmail.com>
2026-06-12 23:56:20 +0530

    Fix api connection

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,15 +1,14 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+
   server: {
-    proxy: {
-      "/api": {
-        target: "https://bank.dhruvcore.com",
-        changeOrigin: true,
-        secure: true,
-      },
-    },
-  },
-});
+    host: "0.0.0.0",
+    allowedHosts: [
+      "bank.dhruvcore.com"
+    ]
+  }
+})
